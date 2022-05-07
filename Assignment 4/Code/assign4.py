d150995a2fec2b6d95a70b8355abbe0c9766512a
Prabhav Singh <bt21btech11004@iith.ac.in>
2022-05-07 14:34:58 +0530

    assignment 4

---
+++ Assignment 4/Code/assign4.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import random as RN 
+#Total possible outcomes
+N=52
+#Number of ace cards of spade
+n_0=1
+#Number of ace cards
+n_1=4
+#Number of black cards 
+n_2=26
+
+#respective probabilities
+pr_0=n_0/N
+pr_1=n_1/N
+pr_2=n_2/N
+
+
+    
+print("Required probabilities are ",pr_0,pr_1,pr_2)
